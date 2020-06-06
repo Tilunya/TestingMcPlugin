8a40122a37e13b740cba276c87a13d8af98aa1c1
Sean Tilundev <sa.tilun.dev@gmail.com>
2020-06-07 00:30:24 +0200

    Clean code for hitZombieEvent

--- TestingPlugin/src/com/tilundev/testingplugin/listeners/ZombieListener.java
+++ TestingPlugin/src/com/tilundev/testingplugin/listeners/ZombieListener.java
@@ -1,17 +1,12 @@
 package com.tilundev.testingplugin.listeners;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.bukkit.entity.EntityType;
-import org.bukkit.entity.LivingEntity;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.CreatureSpawnEvent;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
-import org.bukkit.potion.PotionEffect;
-import org.bukkit.potion.PotionEffectType;
 
+import com.tilundev.testingplugin.listeners.functional.ZombieEvent;
 import com.tilundev.testingplugin.util.ZombieEnum;
 
 public class ZombieListener implements Listener {
@@ -30,18 +25,7 @@ public class ZombieListener implements Listener {
 	@EventHandler
 	public void onEntityDamageByEntity(EntityDamageByEntityEvent ede) {
 		if(ZombieEnum.isContain(ede.getDamager().getType()) && ede.getEntity().getType().equals(EntityType.PLAYER)) {
-			LivingEntity player = (LivingEntity) ede.getEntity();
-			if(Math.random()*100 <50) {
-				player.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 60, 2));
-			}
-
-			if(Math.random()*100 <80) {
-				player.addPotionEffect(new PotionEffect(PotionEffectType.CONFUSION, 200, 2));
-			}
-
-			if(Math.random()*100 <15) {
-				player.addPotionEffect(new PotionEffect(PotionEffectType.WITHER, 100, 5));
-			}
+			ZombieEvent.hitZombieEffect(ede);
 		}
 	}
 
