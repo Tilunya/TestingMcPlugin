7346b91d5fd61b27d782fe1581fadd725f41a8c0
Sean Tilundev <sa.tilun.dev@gmail.com>
2020-06-07 00:41:14 +0200

    Original commit Beta Version 0.1

--- TestingPlugin/src/com/tilundev/testingplugin/listeners/ZombieListener.java
+++ TestingPlugin/src/com/tilundev/testingplugin/listeners/ZombieListener.java
@@ -13,13 +13,6 @@ public class ZombieListener implements Listener {
 	
 	@EventHandler
 	public void onCreatureSpawn(CreatureSpawnEvent creature) {
-//		if (creature.getEntityType().equals(EntityType.ZOMBIE)) {
-//			creature.getEntity().addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 900000, 3)); // 20 Tick => 1 sec
-////			System.out.println("One Zombie Affected with SPEED 3  900000 ticks");
-//		}
-//		else {
-////			System.out.println("None affected");
-//		}
 	}
 	
 	@EventHandler
