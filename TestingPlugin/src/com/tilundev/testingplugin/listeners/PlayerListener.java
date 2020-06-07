bae546907a1faf4cec7ddfe6d703436c07b1fde6
BuildTools <unconfigured@null.spigotmc.org>
2020-06-07 18:05:33 +0200

    Add broken leg and broken harm

--- TestingPlugin/src/com/tilundev/testingplugin/listeners/PlayerListener.java
+++ TestingPlugin/src/com/tilundev/testingplugin/listeners/PlayerListener.java
@@ -4,10 +4,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.bukkit.Material;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 import com.tilundev.testingplugin.scoreboard.ScoreboardObjectives;
 
@@ -47,6 +54,33 @@ public class PlayerListener implements Listener {
 		}
 	}
 	
+	/**
+	 * Whenever player lose health after falling from a high place, he gets a broken leg.
+	 * After losing at least 5 hearts from a fall, the player will gets a permanent slow effect.
+	 * @param entityDamaged
+	 */
+	@EventHandler
+	public void onPlayerFall(EntityDamageEvent entityDamaged) {
+		LivingEntity player = (LivingEntity) entityDamaged.getEntity();
+		if(entityDamaged.getCause().equals(DamageCause.FALL) && entityDamaged.getDamage() > 5) {
+			player.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, Integer.MAX_VALUE, 0));
+			//add "broken leg" to the status scoreboard of the player.
+		}
+	}
+	
+	/**
+	 * Whenever player lose a high amount of health from a hit, he gets a broken harm.
+	 * After losing at least 5 hearts from a hit, the player will gets a permanent mining fatigue effect.
+	 * @param entityHit
+	 */
+	@EventHandler
+	public void onPlayerHit(EntityDamageByEntityEvent entityHit) {
+		LivingEntity player = (LivingEntity) entityHit.getEntity();
+		if((entityHit.getCause().equals(DamageCause.ENTITY_ATTACK) || entityHit.getCause().equals(DamageCause.PROJECTILE)) && entityHit.getDamage() > 5){
+			player.addPotionEffect(new PotionEffect(PotionEffectType.SLOW_DIGGING, Integer.MAX_VALUE, 0));
+			//add "broken harm" to the status scoreboard of the player.
+		}
+	}
 	
 
 }
